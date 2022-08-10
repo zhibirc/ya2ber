35e06707be54297bfb6d2b5127f4a54e8156c5aa
Yaroslav Surilov <ysurilov@lohika.com>
2022-08-10 10:40:53 +0300

    fix SQL code style

--- server/src/database/init.sql
+++ server/src/database/init.sql
@@ -2,11 +2,11 @@ CREATE TABLE users (
     user_id       SERIAL PRIMARY KEY,
     user_name     VARCHAR(30) NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
-    user_role     TEXT NOT NULL DEFAULT 'user', -- assumed, that there will be 2 roles: 'user' and 'admin'
+    user_role     TEXT DEFAULT 'user' NOT NULL, -- assumed, that there will be 2 roles: 'user' and 'admin'
     signup_date   timestamptz NOT NULL,         -- registration date
     last_visit    timestamptz NOT NULL,
     last_ip       inet,                         -- IP address used by user on last visit
-    active        BOOL NOT NULL DEFAULT TRUE    -- special flag depended on application business logic
+    active        BOOL DEFAULT TRUE NOT NULL    -- special flag depended on application business logic
 );
 
 CREATE INDEX users_username_index ON users (user_name);
